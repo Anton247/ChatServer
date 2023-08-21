a0cd811922d8d09d7e1a5c0aea3441d00114e6fc
Anton <kondratov247@mail.ru>
2023-08-21 19:15:54 +0700

    Simple Server

--- ChatServer.cpp
+++ ChatServer.cpp
@@ -1,35 +1,79 @@
 ﻿#include <iostream>
 #include <uwebsockets/App.h>
 #include <nlohmann/json.hpp>
+using namespace std;
+using json = nlohmann::json;
+
+// Публичные сообщения 
+// user13 => server: {"command": "public_msg", "text": "Всем приветы в этом чате"}
+// server => all users {"command": "public_msg", "text": "...", user_from: 13}
+// data, ws
+
+void process_public_msg(auto data, auto* ws) {
+    json payload = {
+        {"command", data["command"]},
+        {"text", data["text"]},
+        {"user_from", ws->getUserData()->user_id}
+    };
+    ws->publish("public_channel", payload.dump());
+}
+
+// Приватные сообщения
+// user10 => server {"command": "private_msg", "text": "...", user_to: 20}
+// server => user20 {"command": "private_msg", "text": "...", user_from: 10}
+void process_private_msg(auto data, auto* ws) {
+    json payload = {
+        {"command", data["command"]},
+        {"text", data["text"]},
+        {"user_from", ws->getUserData()->user_id},
+    };
+    int user_to = data["user_to"];
+    ws->publish("user" + to_string(user_to), payload.dump());
+}
+
+// Возможность указать имя command:set_name
+// Оповещение о подключениях command:status (список людей онлайн)
 
 int main()
 {
-    struct PerSocketData {
-        /* Fill with user data */
+    struct UserData {
+        int user_id;
+        string name;
     };
 
-    /* Keep in mind that uWS::SSLApp({options}) is the same as uWS::App() when compiled without SSL support.
-     * You may swap to using uWS:App() if you don't need SSL */
-    uWS::App().ws<PerSocketData>("/*", {
-            /* Settings */
-            .idleTimeout = 16,
-            /* Handlers */
-            .open = [](auto*/*ws*/) {
-                /* Open event here, you may access ws->getUserData() which points to a PerSocketData struct */
+    int latest_user_id = 10;
 
-            },
-            .message = [](auto* ws, std::string_view message, uWS::OpCode opCode) {
-                /* This is the opposite of what you probably want; compress if message is LARGER than 16 kb
-                 * the reason we do the opposite here; compress if SMALLER than 16 kb is to allow for
-                 * benchmarking of large message sending without compression */
-                ws->send(message, opCode, true);
-            },
-            .close = [](auto*/*ws*/, int /*code*/, std::string_view /*message*/) {
-                /* You may access ws->getUserData() here */
+
+    uWS::App().ws<UserData>("/*", {
+        .idleTimeout = 300,
+        // Вызывается при новом подключении
+        .open = [&latest_user_id](auto* ws) {
+            UserData* data = ws->getUserData();
+            data->user_id = latest_user_id++;
+            data->name = "noname";
+
+            cout << "New user connected: " << data->user_id << endl;
+            ws->subscribe("public_channel"); // подписываем всех на канал(топик) паблик
+            ws->subscribe("user" + to_string(data->user_id)); // подписываем на персональны топик
+        },
+        // При получении данных от клиента
+        .message = [](auto* ws, std::string_view data, uWS::OpCode opCode) {
+            json parsed_data = json::parse(data); // ToDo: check format / handle exception
+
+            if (parsed_data["command"] == "public_msg") {
+                process_public_msg(parsed_data, ws);
             }
-            }).listen(9001, [](auto* listen_socket) {
-                if (listen_socket) {
-                    std::cout << "Listening on port " << 9001 << std::endl;
-                }
-                }).run();
+
+            if (parsed_data["command"] == "private_msg") {
+                process_private_msg(parsed_data, ws);
+            }
+        },
+        .close = [](auto*/*ws*/, int /*code*/, std::string_view /*message*/) {
+            /* You may access ws->getUserData() here */
+        }
+        }).listen(9001, [](auto* listen_socket) {
+            if (listen_socket) {
+                std::cout << "Listening on port " << 9001 << std::endl;
+            } // http://localhost:9001/
+            }).run();
 }
